9c686c7e31ef629a6c1dc266c7f821110989b7d3
kcossio <40069053+kcossio@users.noreply.github.com>
2023-12-06 18:11:22 -0800

    gsap scripts

--- assets/animations-gsap.js
+++ assets/animations-gsap.js
@@ -1,2 +1,16 @@
-console.log('Testing external JS file');
-jQuery('body').addClass('panda');
+// --------------------------------- GSAP/ScrollTrigger - scrolling and tweens ----------------------------- //
+
+    // Register Plugins
+    gsap.registerPlugin(ScrollTrigger); // register the ScrollTrigger plugin first 
+
+    gsap.config({
+      autoSleep: 60,
+      force3D: "auto",
+      nullTargetWarn: false,
+        invalidateOnRefresh: true,
+        //markers:true
+    });
+
+// Run jQuery functions
+(function( $ ) {
+});

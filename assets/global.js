2a53f001bcd1b8135b2a9322695c56bd2f4d28e3
Karl Cossio <karl@kymerastudio.com>
2024-02-19 13:47:34 -0800

    details modal

--- assets/global.js
+++ assets/global.js
@@ -147,7 +147,15 @@ function removeTrapFocus(elementToFocus = null) {
 
 // Details on key up escape
 function onKeyUpEscape(event) {
+  if (event.code.toUpperCase() !== 'ESCAPE') return;
 
+  const openDetailsElement = event.target.closest('details[open]');
+  if (!openDetailsElement) return;
+
+  const summaryElement = openDetailsElement.querySelector('summary');
+  openDetailsElement.removeAttribute('open');
+  summaryElement.setAttribute('aria-expanded', false);
+  summaryElement.focus();
 }
 
 class QuantityInput extends HTMLElement {

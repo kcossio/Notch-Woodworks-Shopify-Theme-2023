d0db671fd07eaa493383162e7ba45fd025bb2fa7
Karl Cossio <karl@kymerastudio.com>
2024-02-27 15:12:49 -0800

    anchor

--- assets/product-sticky-menu.js
+++ assets/product-sticky-menu.js
@@ -1,7 +1,8 @@
 const anchorPoints = document.querySelectorAll(".anchor-point");
 
+
 for (var i = 0; i < anchorPoints.length; i++) {
-    const anchorName = anchorPoints[i].getAttribute("name");
-    const anchorID = anchorPoints[i].getAttribute("id");
+    var anchorName = anchorPoints[i].getAttribute("data-name");
+    var anchorID = anchorPoints[i].getAttribute("id");
     console.log('Anchor Point Name: ' + anchorName + ' ID: ' + anchorID);
 }
